bbaf1a7b84dfae9ee74c73a66ad7644ef92bf674
phuong <phuong@wolffun>
2022-08-07 17:28:26 +0700

    enhance safeGo

--- safe.go
+++ safe.go
@@ -2,16 +2,30 @@ package gop
 
 import (
 	"fmt"
+	"reflect"
 	"runtime/debug"
 )
 
-func SafeGo(f func()) {
-	defer Recover()
+// SafeGo is designed for preventing panic and returning error instead
+func SafeGo(f func()) (err error) {
+	defer RecoverErr(&err)
+
 	f()
+	return
 }
 
+// Recover recovers from panicking and print error + its stacktrace
 func Recover() {
 	if r := recover(); r != nil {
-		fmt.Println("stacktrace from panic: \n" + string(debug.Stack()))
+		err := fmt.Errorf("panic: %v\nstacktrace from panic: %v", r, string(debug.Stack()))
+		fmt.Println(err.Error())
+	}
+}
+
+// RecoverErr returns error that it caught while recovering from panic process
+func RecoverErr(err *error) {
+	if r := recover(); r != nil {
+		catch := fmt.Errorf("panic: %v\nstacktrace from panic: %v", r, string(debug.Stack()))
+		reflect.ValueOf(err).Elem().Set(reflect.ValueOf(catch))
 	}
 }
